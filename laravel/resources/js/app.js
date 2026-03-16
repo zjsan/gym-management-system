e4f6fec4a1048fde7441d8f6a1bb9879654e19e6
zjsan <zjsantos25@gmail.com>
2026-03-16 16:34:23 +0800

    installed frontend dependencies

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import "./bootstrap";
+import "./bootstrap";
+import { createApp } from "vue";
+import { createPinia } from "pinia"; // Import createPinia
+import App from "./components/App.vue"; // Assuming you have an App component
+
+const pinia = createPinia(); // Create the Pinia instance
+const app = createApp(App); // Create the Vue app instance
+
+app.use(pinia); // Tell the app to use Pinia
+// If you use Vue Router, you would also add:
+// import router from './router';
+// app.use(router);
+
+app.mount("#app"); // Mount the app to the #app element in your Blade template
