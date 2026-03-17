f46d2edf4373292b3871b336c4b697d46dafaf39
zjsan <zjsantos25@gmail.com>
2026-03-17 16:04:04 +0800

    updating vite.config.js

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -10,4 +10,10 @@ export default defineConfig({
         }),
         vue(),
     ],
+    server: {
+        host: "0.0.0.0", // Allow connections from outside the container
+        hmr: {
+            host: "localhost",
+        },
+    },
 });
