e4d7f3af51c9d7e0500ba1d92b395540cd57b502
zjsan <zjsantos25@gmail.com>
2026-03-17 15:30:21 +0800

    clearing some content from template

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -8,8 +8,4 @@ const pinia = createPinia(); // Create the Pinia instance
 const app = createApp(App); // Create the Vue app instance
 
 app.use(pinia); // Tell the app to use Pinia
-// If you use Vue Router, you would also add:
-// import router from './router';
-// app.use(router);
-
 app.mount("#app"); // Mount the app to the #app element in your Blade template
