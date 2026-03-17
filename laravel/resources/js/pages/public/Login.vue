24e677ded18b08e63afedc2813e2da4e91f09b3a
zjsan <zjsantos25@gmail.com>
2026-03-17 15:48:24 +0800

    structing frontend pages

---
+++ laravel/resources/js/pages/public/Login.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>login</h1>
+    <form @submit.prevent="submit">
+        <input type="email" v-model="email" placeholder="Email" />
+        <input type="password" v-model="password" placeholder="Password" />
+        <button type="submit">Login</button>
+    </form>
+</template>
