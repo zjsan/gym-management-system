44ad70baa02853a5276d74beb8c93944160d3080
zjsan <zjsantos25@gmail.com>
2026-03-18 16:14:18 +0800

    adding tailwind

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,7 @@ export default defineConfig({
             refresh: true,
         }),
         vue(),
+        tailwindcss(),
     ],
     server: {
         host: "0.0.0.0", // Allow connections from outside the container
