7760f370f9d0d3164fd22d4ad62c7af44d153801
zjsan <zjsantos25@gmail.com>
2026-03-19 16:26:10 +0800

    configured sanctum middleware

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
