12803f43ef3b8615f4bf4e15c1846e7b32a369d4
zjsan <zjsantos25@gmail.com>
2026-03-21 17:12:46 +0800

    added instance header

--- laravel/resources/js/api/api.js
+++ laravel/resources/js/api/api.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'http://localhost:8000' + '/api',
+    headers: {
+        "X-Requested-With": "XMLHttpRequest",
+    },
 })
 
 api.defaults.withCredentials = true;
