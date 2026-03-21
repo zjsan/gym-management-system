6abaf06a084c317f0fa48ac9ac3dbc94a68786e0
zjsan <zjsantos25@gmail.com>
2026-03-21 17:11:23 +0800

    updted axios instance and created auth pinia store

--- laravel/resources/js/api/api.js
+++ laravel/resources/js/api/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: 'http://localhost:8000' + '/api'})
+const api = axios.create({
+    baseURL: 'http://localhost:8000' + '/api',
+})
 
 api.defaults.withCredentials = true;
 api.defaults.withXSRFToken = true;
+
+export default api;
