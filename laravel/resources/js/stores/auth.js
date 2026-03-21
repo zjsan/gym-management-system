d287f3c7928a5995572bbfd683b52434cdbb8c24
zjsan <zjsantos25@gmail.com>
2026-03-21 18:14:23 +0800

    creating login function api call

--- laravel/resources/js/stores/auth.js
+++ laravel/resources/js/stores/auth.js
@@ -7,7 +7,7 @@ export const useAuthStore = defineStore('alerts', {
   state: () => {
     user = ref(null) || JSON.parse(localStorage.getItem("user")) || null, // stores logged-in user object
     loading = false
-    error = false
+    error = null
   },
 
     /**
@@ -18,7 +18,30 @@ export const useAuthStore = defineStore('alerts', {
   getters:{
     isAuthenticated: (state) => !!state.user,
   },
+
   actions: {
+
+     // Save user in localStorage
+    saveUserToStorage() {
+        localStorage.setItem("user", JSON.stringify(this.user)); 
+    },
+  
+    //login 
+    async login(credentials) {
+        this.loading = true
+        this.error = null
+         /* API call & set user */
+         try {
+            
+
+            api.get('/sanctum/csrf-cookie');//intialize csrf protection
+
+            await api.post('/login',credentials)
+            
+         } catch (error) {
+            
+         }
+     },
 
   },
 
