66b837c2b9796a3e5045376e9198839a784e1b43
zjsan <zjsantos25@gmail.com>
2026-03-23 16:36:13 +0800

    added safety check in the login process

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -5,25 +5,39 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     //
     public function login(Request $request): RedirectResponse
     {
+        //validate the incoming request data
         $credentials = $request->validate([
             'email' => ['required', 'email', 'max:255'],
             'password' => 'required|string|min:8',
         ]);
  
+        //check if the user exists in the database
+        $user = User::where($loginField, $request->login)->first();
+
+        //if the user does not exist or the password is incorrect, return an error response
+        if (! $user || ! Hash::check($request->password, $user->password)) 
+        {
+            
+            return response()->json(['message' => 'Invalid credentials.', 401]);
+           
+        }
+
+        //if the user exists and the password is correct, generate the session
         if (Auth::attempt($credentials)) {
+
+            //generate the session for the user
             $request->session()->regenerate();
  
-            return redirect()->intended('dashboard');
+            return response()->json(['message' => 'Login successful.', 200]);
         }
- 
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ])->onlyInput('email');
     }
+ 
 }
