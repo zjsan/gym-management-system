bc254c06f1c7f51abc8628eecdc36928f2952116
zjsan <zjsantos25@gmail.com>
2026-03-24 15:46:55 +0800

    created logic for adding dummy data

--- laravel/database/seeders/UserSeeder.php
+++ laravel/database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'), // The dummy password will be 'password'
+            // Add any other necessary fields
+        ]);
     }
 }
