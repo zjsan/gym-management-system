00489634d3b352f0a5ceb7064ef5e0e90ec0cf6d
zjsan <zjsantos25@gmail.com>
2026-03-27 14:53:14 +0800

    updated login function in auth controller

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -18,17 +15,6 @@ class AuthController extends Controller
             'email' => ['required', 'email', 'max:255'],
             'password' => 'required|string|min:8',
         ]);
- 
-        //check if the user exists in the database
-        $user = User::where($credentials, $request->login)->first();
-
-        //if the user does not exist or the password is incorrect, return an error response
-        if (! $user || ! Hash::check($request->password, $user->password)) 
-        {
-            
-            return response()->json(['message' => 'Invalid credentials.', 401]);
-           
-        }
 
         //if the user exists and the password is correct, generate the session
         if (Auth::attempt($credentials)) {
@@ -36,8 +22,14 @@ class AuthController extends Controller
             //generate the session for the user
             $request->session()->regenerate();
  
-            return response()->json(['message' => 'Login successful.', 200]);
+            return response()->json([
+                'message' => 'Login successful.'
+            ], 200);
         }
+
+        return response()->json([
+            'message' => 'Invalid credentials.'
+        ], 401);
     }
 
 
