f65872de7a52db0002f6fac6f7c6f923177c096f
zjsan <zjsantos25@gmail.com>
2026-03-30 16:18:09 +0800

    create dashboard page template

---
+++ laravel/resources/js/pages/protected/Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        <h1>Dashboard</h1>
+        <p>
+            Welcome to the dashboard! This page is protected and requires
+            authentication.
+        </p>
+    </div>
+</template>
+<script setup></script>
