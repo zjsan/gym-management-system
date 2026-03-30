c75278965103a0634218e916caad2db49db8ee42
zjsan <zjsantos25@gmail.com>
2026-03-30 16:30:03 +0800

    updating frotend routes

--- laravel/resources/js/router/router.js
+++ laravel/resources/js/router/router.js
@@ -1,8 +1,12 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 
 import Login from "../pages/public/Login.vue";
+import Dashboard from "../pages/protected/Dashboard.vue";
 
-const routes = [{ path: "/", component: Login }];
+const routes = [
+    { path: "/", component: Login, name: "Login" },
+    { path: "/dashboard", component: Dashboard, name: "Dashboard" },
+];
 
 export const router = createRouter({
     history: createWebHashHistory(),
