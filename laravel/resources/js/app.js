c1b8e93700f1571514b77e2d4468584658d8fb2b
zjsan <zjsantos25@gmail.com>
2026-03-31 20:17:36 +0800

    updating app.js to include router instance

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -2,10 +2,12 @@ import "./bootstrap";
 import "./bootstrap";
 import { createApp } from "vue";
 import { createPinia } from "pinia"; // Import createPinia
+import router from "./router"
 import App from "./pages/App.vue";
 
 const pinia = createPinia(); // Create the Pinia instance
 const app = createApp(App); // Create the Vue app instance
 
 app.use(pinia); // Tell the app to use Pinia
+app.use(router);
 app.mount("#app"); // Mount the app to the #app element in your Blade template
