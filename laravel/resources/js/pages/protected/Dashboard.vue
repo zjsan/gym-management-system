fcbedca15eaa8f0051f6637bc1326fa815271b99
zjsan <zjsantos25@gmail.com>
2026-03-31 19:19:26 +0800

    adding logout button in the dashboard

--- laravel/resources/js/pages/protected/Dashboard.vue
+++ laravel/resources/js/pages/protected/Dashboard.vue
@@ -5,6 +5,18 @@
             Welcome to the dashboard! This page is protected and requires
             authentication.
         </p>
+
+        <button class="bg-red-500 text-white font-semibold rounded-lg shadow hover:bg-red-600 transition">
+            Logout
+        </button>
     </div>
 </template>
-<script setup></script>
+<script setup>
+import { useAuthStore } from '../../stores/auth';
+
+const auth = useAuthStore();
+
+const logout = async () => {
+    await auth.logout();
+};
+</script>
