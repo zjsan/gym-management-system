0b234a763d238238223e5bd75ebe6ff932219bdf
zjsan <zjsantos25@gmail.com>
2026-04-02 14:33:12 +0800

    created page template for admin and staff

---
+++ laravel/resources/js/pages/protected/Admin/Adminview.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1>Admin Dashboard</h1>
+        <p>
+            Welcome to the Admin dashboard! This page is protected and requires
+            authentication and only for admin.
+        </p>
+
+        <button @click="logout" class="bg-red-500 text-white font-semibold rounded-lg shadow hover:bg-red-600 transition">
+            Logout
+        </button>
+    </div>
+</template>
+<script setup>
+import { useAuthStore } from '../../stores/auth';
+
+const auth = useAuthStore();
+
+const logout = async () => {
+    await auth.logout();
+};
+</script>
---
+++ laravel/resources/js/pages/protected/Staff/Staffview.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1>Staff Dashboard</h1>
+        <p>
+            Welcome to the Staff dashboard! This page is protected and requires
+            authentication and only for staff.
+        </p>
+
+        <button @click="logout" class="bg-red-500 text-white font-semibold rounded-lg shadow hover:bg-red-600 transition">
+            Logout
+        </button>
+    </div>
+</template>
+<script setup>
+import { useAuthStore } from '../../stores/auth';
+
+const auth = useAuthStore();
+
+const logout = async () => {
+    await auth.logout();
+};
+</script>
