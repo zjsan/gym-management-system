f493c4672d7d96636c86c041aa58d62f45a58cf9
zjsan <zjsantos25@gmail.com>
2026-04-13 16:29:12 +0800

    updating router instance

--- laravel/resources/js/router/router.js
+++ laravel/resources/js/router/router.js
@@ -2,10 +2,22 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import Login from "../pages/public/Login.vue";
 import Dashboard from "../pages/protected/Dashboard.vue";
+import Adminview from "../pages/Admin/Adminview.vue";
 
 const routes = [
     { path: "/", component: Login, name: "Login" },
-    { path: "/dashboard", component: Dashboard, name: "Dashboard" },
+    {
+        path: "/dashboard",
+        component: Dashboard,
+        name: "Dashboard",
+        meta: { requiresAdmin: true },
+    },
+    {
+        path: "/admin-page",
+        component: Adminview,
+        name: "Adminview",
+        meta: { requiresAdmin: true, role: "admin" },
+    },
 ];
 
 const router = createRouter({
