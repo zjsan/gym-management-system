392d27e27734f220aee1b862e3718ed46a474ca9
zjsan <zjsantos25@gmail.com>
2026-04-20 16:23:31 +0800

    registered middleware in bootstrap/app.php

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\AdminMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
         $middleware->statefulApi();
+        $middleware->append(AdminMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
