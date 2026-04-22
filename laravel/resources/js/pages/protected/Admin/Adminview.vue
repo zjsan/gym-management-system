ddc74304a7d6ed129cd673581c3c16d290f3af41
zjsan <zjsantos25@gmail.com>
2026-04-22 16:15:07 +0800

    creating sample form for admin page

--- laravel/resources/js/pages/protected/Admin/Adminview.vue
+++ laravel/resources/js/pages/protected/Admin/Adminview.vue
@@ -1,9 +1,30 @@
 <template>
     <h1>Admin Dashboard</h1>
-     <p>
-            Welcome to the Admin dashboard! This page is protected and requires
-            authentication and only for admin.
+    <p>
+        Welcome to the Admin dashboard! This page is protected and requires
+        authentication and only for admin.
     </p>
+
+    <div>
+        <form @submit.prevent="handleSubmit">
+            <input v-model="form.name" type="text" placeholder="Full Name" />
+            <input
+                v-model="form.email"
+                type="email"
+                placeholder="Email Address"
+            />
+
+            <select v-model="form.role">
+                <option value="staff">Staff</option>
+                <option value="admin">Admin</option>
+            </select>
+
+            <button :disabled="userStore.loading">Save User</button>
+
+            <p v-if="userStore.errors" class="text-red-500">
+                {{ userStore.errors.email[0] }}
+            </p>
+        </form>
+    </div>
 </template>
-<script setup>
-</script>
+<script setup></script>
