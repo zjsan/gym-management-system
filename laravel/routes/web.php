574a1f8987ba3ad7ef2460615ffa03e3d4bbc6bf
zjsan <zjsantos25@gmail.com>
2026-04-22 16:09:04 +0800

    fixed 403 problem

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/{any}', function () {
     return view('welcome'); // or 'app' if you created app.blade.php
 })->where('any', '.*');
+
+Route::get('/admin', function () {
+    // ...
+})->middleware(AdminMiddleware::class);
