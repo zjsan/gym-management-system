bdb5e262daa937e80ac0681200704951ed073d9f
zjsan <zjsantos25@gmail.com>
2026-04-25 14:27:52 +0800

    fixed role import error

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;    
 use Illuminate\Support\Facades\Hash;
+use App\Models\Role;   
 
 class UserController extends Controller
 {
