366c2a0f1dccf4b0968a8acca23395da039baf4a
zjsan <zjsantos25@gmail.com>
2026-05-01 20:09:00 +0800

    created index function operation for members

--- laravel/app/Http/Controllers/MemberController.php
+++ laravel/app/Http/Controllers/MemberController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Member;   
 
 class MemberController extends Controller
 {
@@ -12,6 +13,7 @@ class MemberController extends Controller
     public function index()
     {
         //
+        return response()->json(Member::all());//eager loading the slug of the role
     }
 
     /**
