9b2e7c1b384ab454d7697c8dc26e2d51b85ded72
zjsan <zjsantos25@gmail.com>
2026-05-05 15:39:46 +0800

    fixed typo in the model

--- laravel/app/Models/Member.php
+++ laravel/app/Models/Member.php
@@ -8,7 +8,7 @@ class Member extends Model
 {
     //
     protected $fillable = [
-        'membersip_no', 'first_name', 'last_name', 'contact_number', 
+        'membership_no', 'first_name', 'last_name', 'contact_number', 
         'address', 'gender', 'photo_path', 'is_active', 
         'membership_start', 'membership_end'
     ];
