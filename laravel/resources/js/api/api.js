53822c9050e3ff79ef1fac664853314678baa6a7
zjsan <zjsantos25@gmail.com>
2026-05-13 16:21:49 +0800

    updated axios instance

--- laravel/resources/js/api/api.js
+++ laravel/resources/js/api/api.js
@@ -1,12 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000' + '/api',
+    baseURL: "http://localhost:8000" + "/api",
     headers: {
         "X-Requested-With": "XMLHttpRequest",
-        "Content-Type": "application/json",
     },
-})
+});
 
 api.defaults.withCredentials = true;
 api.defaults.withXSRFToken = true;
