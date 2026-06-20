b5a85d2f1f1890cbf7004b4f0fb7989db2b5af59
zjsan <zjsantos25@gmail.com>
2026-06-20 18:35:45 +0800

    updated member migration file

--- laravel/database/migrations/2026_05_01_101900_create_members_table.php
+++ laravel/database/migrations/2026_05_01_101900_create_members_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
-            $table->string('membership_no')->unique(); // e.g., GYM-001
+            $table->string('membership_no')->nullable()->unique(); // e.g., GYM-001
             $table->string('first_name');
             $table->string('last_name');
             $table->string('contact_number');
