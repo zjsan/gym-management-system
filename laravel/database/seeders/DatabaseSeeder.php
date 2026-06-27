eed884f75e392ea5dde48869bbf3e63f54d16887
zjsan <zjsantos25@gmail.com>
2026-06-27 20:11:27 +0800

    updated the database seeder to include seeding the members

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
              RoleSeeder::class,
              UserSeeder::class,
             // You can add other seeders here
+             MemberSeeder::class,
         ]);
     }
 }
