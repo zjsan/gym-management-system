3a4f91eab33482db9db34c6fc80751b001950bad
zjsan <zjsantos25@gmail.com>
2026-07-19 15:06:53 +0800

    created the walkin and attendance logging model

---
+++ laravel/app/Models/Walkin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\AttendanceLogging;
+
+class Walkin extends Model
+{
+    protected $fillable = ['name', 'fee'];
+
+    public function attendanceLogs(): HasMany
+    {
+        return $this->hasMany(AttendanceLogging::class);
+    }
+}
