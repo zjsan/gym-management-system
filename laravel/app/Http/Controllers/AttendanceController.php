c9650b267b16d3dc91bb4be0ffddc74e5abe8e37
zjsan <zjsantos25@gmail.com>
2026-07-19 15:43:46 +0800

    created backend index function

--- laravel/app/Http/Controllers/AttendanceController.php
+++ laravel/app/Http/Controllers/AttendanceController.php
@@ -14,12 +14,18 @@ use Exception;
 
 class AttendanceController extends Controller
 {
+  
     /**
-     * Display a listing of the resource.
+     * Display a listing of today's attendance logs (Live Feed).
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        $logs = AttendanceLogging::with(['member', 'walkin', 'recorder'])
+            ->whereDate('check_in', Carbon::today())
+            ->orderBy('check_in', 'desc')
+            ->get();
+
+        return response()->json($logs);
     }
 
     /**
