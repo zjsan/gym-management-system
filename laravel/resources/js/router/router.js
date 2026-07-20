74978a686df81c9d1b86c2d8c3d64c4322961e01
zjsan <zjsantos25@gmail.com>
2026-07-20 16:19:32 +0800

    added new routes in vue router

--- laravel/resources/js/router/router.js
+++ laravel/resources/js/router/router.js
@@ -3,6 +3,8 @@ import { createWebHistory, createRouter } from "vue-router";
 import Login from "../pages/public/Login.vue";
 import Dashboard from "../pages/protected/Main.vue";
 import Adminview from "../pages/protected/Admin/Adminview.vue";
+import MemberManagement from "../pages/protected/Staff/Staffview.vue";
+import AttendanceDashboard from "../pages/protected/Staff/AttendanceDashboard.vue";
 
 const routes = [
     { path: "/", component: Login, name: "Login", requiresAuth: false },
@@ -17,6 +19,18 @@ const routes = [
         component: Adminview,
         name: "Adminview",
         meta: { requiresAdmin: true, role: "admin" },
+    },
+    {
+        path: "/member-management",
+        component: MemberManagement,
+        name: "MemberManagement",
+        meta: { requiresAdmin: true, role: "staff" },
+    },
+    {
+        path: "/attendance",
+        name: "attendance.dashboard",
+        component: AttendanceDashboard,
+        meta: { requiresAdmin: true, role: "staff" },
     },
 ];
 
