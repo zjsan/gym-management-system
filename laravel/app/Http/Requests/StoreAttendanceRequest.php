14738d698481da6ff0bed2affa32b0f132e28261
zjsan <zjsantos25@gmail.com>
2026-07-25 16:11:06 +0800

    fixed problem in the attendance backend form request

--- laravel/app/Http/Requests/StoreAttendanceRequest.php
+++ laravel/app/Http/Requests/StoreAttendanceRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreAttendanceRequest extends FormRequest
 {
@@ -12,7 +13,7 @@ class StoreAttendanceRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
