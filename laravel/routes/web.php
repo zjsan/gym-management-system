4c071474fa3627bc314fc431ddd1e936f017e52a
zjsan <zjsantos25@gmail.com>
2026-08-24 16:30:04 +0800

    added a temporary dev route for tweaking the qr code page

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AdminMiddleware;
+use App\Models\Member;
+use App\Mail\MemberQrCodeMail;
+
+if (app()->isLocal()) {
+    Route::get('/dev/qr-email-preview', function () {
+        $member = Member::first() ?? new Member([
+            'first_name' => 'Dez',
+            'email' => 'oragelemon25@gmail.com',
+            'qr_token' => 'GYM-TEST-12345'
+        ]);
+        return new MemberQrCodeMail($member);
+    });
+}
 
 Route::get('/{any}', function () {
     return view('welcome'); // or 'app' if you created app.blade.php
