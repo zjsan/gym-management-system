cd96823bf0b51e219f01ff05858d0ab17caac644
zjsan <zjsantos25@gmail.com>
2026-08-28 16:26:09 +0800

    installed needed packages

---
+++ laravel/resources/js/components/ui/dialog/DialogFooter.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { DialogClose } from "reka-ui";
+import { cn } from "@/lib/utils";
+import { Button } from '@/components/ui/button';
+
+const props = defineProps({
+  class: {
+    type: [Boolean, null, String, Object, Array],
+    required: false,
+    skipCheck: true,
+  },
+  showCloseButton: { type: Boolean, required: false, default: false },
+});
+</script>
+
+<template>
+  <div
+    data-slot="dialog-footer"
+    :class="
+      cn('flex flex-col-reverse gap-2 sm:flex-row sm:justify-end', props.class)
+    "
+  >
+    <slot />
+    <DialogClose v-if="showCloseButton" as-child>
+      <Button variant="outline"> Close </Button>
+    </DialogClose>
+  </div>
+</template>
