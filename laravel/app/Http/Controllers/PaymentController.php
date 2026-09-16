af868a1f598a6c408b3bb9dbf2a87e9e6978c0e7
zjsan <zjsantos25@gmail.com>
2026-09-16 16:27:52 +0800

    fixed error in the sql query by correcting the eagerloading syntax

--- laravel/app/Http/Controllers/PaymentController.php
+++ laravel/app/Http/Controllers/PaymentController.php
@@ -98,7 +98,7 @@ class PaymentController extends Controller
         $query = Payment::with([
             'member:id,first_name,last_name,membership_no',
             'walkin:id,name',
-            'processedBy:id,first_name,last_name,'
+            'processedBy:id,first_name,last_name'
         ])->latest('paid_at');
 
         // Apply exact same filters as index()
